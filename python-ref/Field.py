332fa666e76beceb536346c8b2af30bb3f1b080a
Shiho Midorikawa <shiho.elliptic@gmail.com>
2016-03-03 12:17:25 +0900

    add abstract field class

---
+++ python-ref/Field.py
@@ -0,0 +1,132 @@
+import math
+from util import egcd, gcd, modinv
+
+class Field:
+  def __init__(s, element_class):
+    s.element_class = element_class
+
+  def __repr__(s):
+    return "Field()"
+
+  def __str__(s):
+    return "Field"
+
+  def __call__(s, x):
+    return s.element_class(s, x)
+
+  def _add(s, a, b):
+    raise NotImplementedError()
+
+  def _mul(s, a, b):
+    raise NotImplementedError()
+
+  def _inv(s, a):
+    raise NotImplementedError()
+
+  def _neg(s, a, b):
+    raise NotImplementedError()
+
+  def _equ(s, a, b):
+    raise NotImplementedError()
+
+  def order(s):
+    return 0
+
+  def _ord(s):
+    return 0
+
+class FieldElement:
+  def __init__(s, field, x):
+    s.field = field
+    s.x = x
+
+  def change_field(s, _field):
+    return s.__class__(_field, s.x)
+
+  def order(s):
+    return s.field._ord(s)
+
+  def __add__(s, rhs):
+    if isinstance(rhs, FieldElement):
+      return s.field._add(s.x, rhs.x)
+    else:
+      return s.field._add(s.x, rhs)
+
+  def __sub__(s, rhs):
+    if isinstance(rhs, FieldElement):
+      return s.field._add(s.x, (-rhs).x)
+    else:
+      return s.field._add(s.x, -rhs)
+
+  def __neg__(s):
+    return s.field._neg(s.x)
+
+  def __mul__(s, rhs):
+    if isinstance(rhs, FieldElement):
+      return s.field._mul(rhs.x, s.x)
+    else:
+      return s.field._mul(rhs, s.x)
+
+  def __div__(s, rhs):
+    if isinstance(rhs, FieldElement):
+      return s.field._mul(rhs.x, s.field._inv(s.x).x)
+    else:
+      return s.field._mul(rhs, s.field._inv(s.x).x)
+
+  def __rdiv__(s, lhs):
+    if isinstance(lhs, FieldElement):
+      return s.field._mul(s.field._inv(s.x).x, lhs.x)
+    else:
+      return s.field._mul(s.field._inv(s.x).x, lhs)
+
+  def __pow__(s, rhs, op="*"):
+    if op not in "+*":
+      raise Exception("Unknown Operator")
+    if rhs == 0:
+      return s.__class__(s.field, 1)
+    if isinstance(rhs, fieldElement):
+      d = rhs.x
+    else:
+      d = rhs
+    bits = map(lambda x: x == "1", bin(d)[2:])[::-1]
+    x = s
+    if bits[0]:
+      res = x
+    else:
+      res = s.__class__(s.field, 1)
+    for cur in bits[1:]:
+      if op == "*":
+        x *= x
+      elif op == "+":
+        x += x
+      if cur:
+        res *= x
+        if op == "*":
+          res *= x
+        elif op == "+":
+          res += x
+    return res
+
+  def __radd__(s, lhs):
+    return s + lhs
+
+  def __rsub__(s, lhs):
+    return -s + lhs
+
+  def __rmul__(s, lhs):
+    return s * lhs
+
+  def __ne__(s, rhs):
+    return not (s == rhs)
+
+  def __eq__(s, rhs):
+    if isinstance(rhs, FieldElement):
+      return s.field._equ(s.x, rhs.x)
+    else:
+      return s.field._equ(s.x, rhs)
+
+  def __repr__(s):
+    return "%r(%s)" % (s.field, s.x)
+
+  def __str__(s):
+    return "%s" % s.x
---
+++ python-ref/RealField.py
@@ -0,0 +1,38 @@
+from Field import Field, FieldElement
+
+class RealField(Field):
+  def __init__(s):
+    Field.__init__(s, RealFieldElement)
+
+  def __repr__(s):
+    return "RealField()"
+
+  def __str__(s):
+    return "RealField()"
+
+  def _add(s, a, b):
+    return s.element_class(s, a+b)
+
+  def _mul(s, a, b):
+    return s.element_class(s, a*b)
+
+  def _inv(s, a):
+    return s.element_class(s, 1/a)
+
+class RealFieldElement(FieldElement):
+  def __init__(s, field, x):
+    FieldElement.__init__(s, field, x)
+
+
+if __name__ == "__main__":
+  RR = RealField()
+  print RR
+  a = RR(1)
+  b = RR(2)
+  print a
+  print b
+  print a+b
+  print a*b
+  print "%r" % (a+b)
+  print RR(5) * RR(65537)
+  print RR(123.456) / 3
