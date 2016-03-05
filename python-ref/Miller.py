8568d0afa9f1500a5da7169838db13af01163b17
Shiho Midorikawa <shiho.elliptic@gmail.com>
2016-03-05 12:40:10 +0900

    remve debug code, use line_coeff

--- python-ref/Miller.py
+++ python-ref/Miller.py
@@ -1,20 +1,8 @@
 from ecpy import *
 
 def miller(E, P, Q, m):
-  def ec_lambda(P, Q):
-    if P.x == Q.x:
-      l = (3*P.x**2 + E.a)
-      m = 2*P.y
-      if m == 0:
-        return Q.x - P.x
-      l = (l * E.field._inv(m)).x
-      return Q.y - P.y - l * (Q.x - P.x)
-    else:
-      return Q.x - P.x
-    return l
-
   def l(P, Q, R):
-    return R.y - (ec_lambda(P, Q) * (R.x - P.x) + P.y)
+    return R.y - ( P.line_coeff(Q) * (R.x - P.x) + P.y)
 
   def v(P, Q):
     return Q.x - P.x
@@ -26,12 +14,12 @@ def miller(E, P, Q, m):
   f = E.field(1)
   T = P
   for x in d[1:]:
-    f = f**2 * g(T, T, Q)
+    f = f**2
+    f *= g(T, T, Q)
     T = 2*T
     if x:
       f = f * g(P, T, Q)
       T = T + P
-    print repr(f)
   return f.x
 
 if __name__ == "__main__":
