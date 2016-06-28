79206c8ff587233150f09e28a95d4193275ce55f
Shiho Midorikawa <shiho.elliptic@gmail.com>
2016-06-28 18:18:04 +0900

    add source code

---
+++ cpp/includes/ecpy_native.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <boost/python.hpp>
+#include <iostream>
+#include <vector>
+
+#include <gmp.h>
+#include <gmpxx.h>
---
+++ cpp/src/ecpy_native.cpp
@@ -0,0 +1,7 @@
+#include "ecpy_native.h"
+
+using namespace std;
+
+BOOST_PYTHON_MODULE(ecpy_native) {
+  using namespace boost::python;
+}
