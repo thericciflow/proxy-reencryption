c1305788e45ab5b2f5d27491e5b9a19992530a49
Shiho Midorikawa <shiho.elliptic@gmail.com>
2016-06-28 17:32:59 +0900

    reset

--- cpp/src/complexfield.cpp
+++
@@ -1,8 +0,0 @@
-#include <ecpy.h>
-
-ComplexField _cf_instance {};
-
-std::ostream& operator<<(std::ostream& os, const ComplexField& field) {
-  os << "Complex Field";
-  return os;
-}
--- cpp/src/util.cpp
+++
@@ -1,6 +0,0 @@
-#include <ecpy.h>
-using namespace std;
-
-bool is_prime(const mpz_class& x) {
-  return mpz_probab_prime_p(MPZ_T(x), 1000);
-}
